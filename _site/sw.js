83339503c0152829d4f93e9ca63b438f78fd8228
killerkenobi <anthonyal@protonmail.com>
2023-05-01 20:07:20 -0400

    commenting out deployment check

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/killerkenobi.github.io/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230501.195024';
+const cacheName = 'chirpy-20230501.200705';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
